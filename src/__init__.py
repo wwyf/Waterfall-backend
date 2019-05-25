de3cbf2ca5c0877307751cd1a61d470f66e47ddd
wyf-book <walkeryf@yeah.net>
2019-05-25 19:13:38 +0800

    update default port

--- src/__init__.py
+++ src/__init__.py
@@ -37,4 +37,4 @@ app.register_blueprint(bp.order_bp)
 app.register_blueprint(bp.wallet_bp)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
