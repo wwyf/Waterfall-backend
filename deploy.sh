a16fc79323530ce24df52f2e99b6d53672607d46
wyf-book <walkeryf@yeah.net>
2019-06-04 11:02:54 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 set -x
 echo $TRAVIS_BRANCH
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    openssl aes-256-cbc -K $encrypted_70ca040df3bb_key -iv $encrypted_70ca040df3bb_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
     eval "$(ssh-agent -s)"
     chmod 600 /tmp/deploy_rsa
     ssh-add /tmp/deploy_rsa
